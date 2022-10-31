b875f31c34e48d0bc0b77263a90a9dce0fe62012
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2022-10-31 10:26:33 +0800

    Panli/add hello world for c (#3)
    * Add hello world for C with linux target.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

---
+++ hello_world_linux.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+  FILE *fp = tmpfile();
+  int fd = fileno(fp);
+
+  printf("Try to write to tmp file with fd %d ...\n", fd);
+
+  fprintf(fp, "Hello, the C programming language with linux!\n");
+
+  printf("Try to sleep 3 seconds\n");
+
+  sleep(3);
+  fclose(fp);
+
+  printf("Completed!\n");
+
+  return EXIT_SUCCESS;
+}
